583fa23c7b334afaee95cdf70cf7b4dfe0ae170c
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-11-13 23:17:59 +0000

    Make carousel manually loopable

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 function nextSlide () {
   const currentSlide = document.querySelector(".showing");
+  currentSlide.classList.remove("showing");
   const nextSlide = currentSlide.nextElementSibling;
   // nextElementSibling returns the element or null if no element
   if (nextSlide) {
-    currentSlide.classList.remove("showing");
     nextSlide.classList.add("showing");
     changeDotDisplay(currentSlide, nextSlide);
+  } else {
+    // If on last slide
+    // Will select the first option
+    const firstSlide = document.querySelector(".carousel-slide");
+    firstSlide.classList.add("showing");
+    changeDotDisplay(currentSlide, firstSlide)
   }
 }
 
 function previousSlide () {
   const currentSlide = document.querySelector(".showing");
+  currentSlide.classList.remove("showing");
   const previousSlide = currentSlide.previousElementSibling;
   if (previousSlide) {
-    currentSlide.classList.remove("showing");
     previousSlide.classList.add("showing");
     changeDotDisplay(currentSlide, previousSlide);
+  } else {
+    // If on first slide
+    const lastSlide = document.querySelector(".carousel-slide:last-child");
+    lastSlide.classList.add("showing");
+    changeDotDisplay(currentSlide, lastSlide);
   }
 }
 
@@ -59,3 +70,5 @@ carousel.addEventListener("click", e => {
     changeDot(e.target);
   }
 })
+
+// TODO: Make slides advance every 5 seconds
